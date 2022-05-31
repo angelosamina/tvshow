1c03383fd35ee56fa0e1385d97e0677da7a2c314
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-05-31 11:20:46 +0200

    Création de la liste des series

--- public/index.php
+++ public/index.php
@@ -2,4 +2,34 @@
 
 declare(strict_types=1);
 
-echo 'Hello Tv-Show!';
+use Database\MyPdo;
+use Html\AppWebPage;
+
+$webPage = new AppWebPage();
+$webPage->setTitle('Séries TV');
+
+$stmt = Collection\TvshowCollection::findAll();
+
+$webPage->appendContent(
+    <<<HTML
+    <list>
+HTML
+);
+
+foreach ($stmt as $res) {
+    $id = $res->getId();
+    $nom = WebPage::escapeString($res->getName());
+    $webPage->appendContent(
+        <<<HTML
+        <a href="/season.php?id=$id">$nom</a>\n
+    HTML
+    );
+}
+
+$webPage->appendContent(
+    <<<HTML
+    </list>
+HTML
+);
+
+echo $webPage->toHTML();
