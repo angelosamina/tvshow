fe938555c48bbbeaa5baa8b27fe175118640fd4f
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-05-31 11:46:50 +0200

    Ajout du fichier poster.php

---
+++ public/poster.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+use Html\AppWebPage;
+
+$webPage = new AppWebPage();
+
+try {
+    if (isset($_GET['posterId'])==false) {
+        throw new ParameterException();
+    }
+} catch (ParameterException) {
+    http_response_code(400);
+    exit();
+}
+
+try {
+    if (ctype_digit($_GET['posterId'])==false) {
+        throw new ParameterException();
+    }
+} catch (ParameterException) {
+    http_response_code(400);
+    exit();
+}
+
+settype($_GET['posterId'], "int");
+try {
+    $cover = Cover::findById($_GET['posterId']);
+} catch (EntityNotFoundException $except) {
+    http_response_code(404);
+    exit();
+}
+
+header("Content-Type: image/jpeg");
+
+echo("{$cover->getJpeg()}");
