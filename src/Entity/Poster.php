a5abf9b3d5107c3994eb5715020b6e44c5884779
Tristan Choquet <tristan.choquet@etudiant.univ-reims.fr>
2022-05-31 11:24:22 +0200

    Ajout de la méthode findById à la classe Poster.php

--- src/Entity/Poster.php
+++ src/Entity/Poster.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use PDO;
+
 class Poster
 {
     private int $id;
@@ -23,5 +26,23 @@ class Poster
     public function getJpeg(): string
     {
         return $this->jpeg;
+    }
+
+    public static function findById(int $id): Poster
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, jpeg
+            FROM cover
+            WHERE id = :id
+        SQL
+        );
+
+        $stmt->execute([':id' => $id]);
+
+        $stmt -> setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Cover::class);
+        $poster =  $stmt->fetch();
+
+        return $poster;
     }
 }
