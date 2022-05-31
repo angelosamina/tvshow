b80cfde6570a8d8c92d34ed5d9e38c5c135a7c52
Tristan Choquet <tristan.choquet@etudiant.univ-reims.fr>
2022-05-31 11:26:40 +0200

    Création de la classe TvshowCollection.php

---
+++ src/Entity/Collection/TvshowCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Tvshow;
+use PDO;
+
+class TvshowCollection
+{
+    public static function findAll() : array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT name, overview, posterId
+            FROM tvshow
+            ORDER BY name
+        SQL
+        );
+
+        $stmt->execute();
+
+        $stmt -> setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Tvshow::class);
+        return $stmt->fetchAll();
+    }
+}
