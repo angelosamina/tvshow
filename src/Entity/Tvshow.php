c343bcc750ae34fe7d930a391cddd30161576ce4
Tristan Choquet <tristan.choquet@etudiant.univ-reims.fr>
2022-05-31 11:04:39 +0200

    Création de la classe Tvshow avec ses getters.

---
+++ src/Entity/Tvshow.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Tvshow
+{
+    private int $id;
+    private string $name;
+    private string $originalName;
+    private string $homepage;
+    private string $overview;
+    protected int $posterId;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOriginalName(): string
+    {
+        return $this->originalName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHomepage(): string
+    {
+        return $this->homepage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+
+}
