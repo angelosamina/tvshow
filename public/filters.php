71c1ba349172d3aedf55577afbf568a809a1a303
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-06-01 15:53:10 +0200

    Ajout d'un formulaire qui permet de selectionner les différents genres

--- public/filters.php
+++ public/filters.php
@@ -1,2 +1,42 @@
 <?php
 declare(strict_types=1);
+
+use Entity\Collection;
+use Html\AppWebPage;
+
+$webPage = new AppWebPage();
+
+$webPage->setTitle(
+    <<<HTML
+        Filtres
+    HTML
+);
+
+$stmt = Collection\GenreCollection::findAll();
+
+$webPage->appendContent(
+    <<<HTML
+        <form name="filters" method="GET" action="check.php" >
+    HTML
+);
+foreach ($stmt as $res) {
+    $id = $res->getId();
+    $name = AppWebPage::escapeString($res->getName());
+    $webPage->appendContent(
+        <<<HTML
+            <label>
+                <input name="id" type="checkbox" value="$id">
+                $name
+            </label>
+    HTML
+    );
+}
+
+$webPage->appendContent(
+    <<<HTML
+            <button type="submit">Envoyer</button>				
+        </form>
+    HTML
+);
+
+echo $webPage->toHTML();
