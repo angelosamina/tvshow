c52fa1ce30abc2ca8343b1d676d0f1dbcc028daf
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-06-01 14:50:42 +0200

    Correction du fichier Tvshow_Genre.php avec l'ajout de la méthode getGenres

--- src/Entity/Tvshow_Genre.php
+++ src/Entity/Tvshow_Genre.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Entity\Collection\GenreCollection;
+
 class Tvshow_Genre
 {
     private int $id;
@@ -32,5 +34,10 @@ class Tvshow_Genre
     public function getTvShowId(): int
     {
         return $this->tvShowId;
+    }
+
+    public function getGenres(int $tvShowId): array
+    {
+        return GenreCollection::findByTvshowId($this->$tvShowId);
     }
 }
