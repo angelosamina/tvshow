8af855c1163dc2ca25abc9d3330f678fcb3d19b7
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-06-01 14:14:28 +0200

    Création de la classe Genre.php

---
+++ src/Entity/Genre.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class Genre
+{
+    private int $id;
+    private string $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+}
