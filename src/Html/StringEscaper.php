21f6dc343baf74d2eec2ecaa651edb1aad3b385a
Tristan Choquet <tristan.choquet@etudiant.univ-reims.fr>
2022-06-01 15:31:34 +0200

    Création du trait StringEscaper.php

---
+++ src/Html/StringEscaper.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Html;
+
+trait StringEscaper
+{
+    /**
+     * @param string $string
+     * @return string
+     */
+    public static function escapeString(?string $string=null): ?string
+    {
+        $chaine = htmlspecialchars($string, ENT_QUOTES | ENT_HTML5, "UTF-8");
+        return $chaine;
+    }
+}
