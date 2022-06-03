8a4723a3c31d1100deb5afdfc554949cdbaf20ce
Tristan Choquet <tristan.choquet@etudiant.univ-reims.fr>
2022-06-03 11:50:11 +0200

    Correction du code

--- public/admin/tvshow-save.php
+++ public/admin/tvshow-save.php
@@ -4,35 +4,35 @@ declare(strict_types=1);
 
 use Exception\ParameterException;
 
-try{
-    if (($_POST['name'] == '') || (isset($_POST['name'])==False)) {
+try {
+    if (($_POST['name'] == '') || (isset($_POST['name'])==false)) {
         throw new ParameterException("Name invalide");
     }
-}catch (ParameterException){
+} catch (ParameterException) {
     http_response_code(400);
 }
 
-try{
-    if (($_POST['originalName'] == '') || (isset($_POST['originalName'])==False)) {
+try {
+    if (($_POST['originalName'] == '') || (isset($_POST['originalName'])==false)) {
         throw new ParameterException("originalName invalide");
     }
-}catch (ParameterException){
+} catch (ParameterException) {
     http_response_code(400);
 }
 
-try{
-    if (($_POST['homepage'] == '') || (isset($_POST['homepage'])==False)) {
+try {
+    if (($_POST['homepage'] == '') || (isset($_POST['homepage'])==false)) {
         throw new ParameterException("homepage invalide");
     }
-}catch (ParameterException){
+} catch (ParameterException) {
     http_response_code(400);
 }
 
-try{
-    if (($_POST['overview'] == '') || (isset($_POST['overview'])==False)) {
+try {
+    if (($_POST['overview'] == '') || (isset($_POST['overview'])==false)) {
         throw new ParameterException("overview invalide");
     }
-}catch (ParameterException){
+} catch (ParameterException) {
     http_response_code(400);
 }
 
--- src/Entity/Tvshow.php
+++ src/Entity/Tvshow.php
@@ -252,5 +252,4 @@ class Tvshow
 
         return $this;
     }
-
 }
